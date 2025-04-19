5ae60e426201581ae40c258cbd8ea5f74dd89153
josegoms <josegomespinheiro26@gmail.com>
2025-04-19 12:48:31 -0300

    Set up files, html boilerplate, css reset, and js pseudocode

---
+++ scripts.js
@@ -0,0 +1,17 @@
+// Gameboard module
+    //Create gameboard with "blank" spaces
+    //Function to get a copy of the gameboard safely (getter)
+    //Check if a move is valid (if cell is empty)
+    //Update the cell with player's move
+    //Reset gameboard
+    //Check if board is full
+
+// Game Controller / Players module
+    //Create two players (could be just symbols "X" and "O" for now)
+    //Handle move: player picks a cell → module checks → updates gameboard
+    //Check for win/loss/tie after every move (using winning index combos)
+    //Switch players automatically after a valid move
+    //Keep track of who's playing (currentPlayer)
+    //Track scores (wins, losses, ties) [good addition!]
+    //Allow resetting game state (board + currentPlayer + scores)
+
